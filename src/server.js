8ce46ee7e1215171c52b732f3afc90cddbabb55d
Oksana <omoskovko98@gmail.com>
2025-09-11 11:26:10 +0300

    created a server to work with a collection of contacts through HTTP requests

---
+++ src/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getContactsByIdController, getContactsController } from './controllers/contacts.js';
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  
+  app.get('/contacts', getContactsController);
+  app.get('/contacts/:contactId', getContactsByIdController);
+
+  app.use((err, req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+      error: err.message,
+    });
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+ 
