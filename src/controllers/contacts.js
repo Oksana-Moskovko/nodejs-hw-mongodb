9f5f31cfa17fc5f7fb2dd7bacd02bb37bf6b531b
Oksana <omoskovko98@gmail.com>
2025-09-21 12:03:04 +0300

    error correction PATCH

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -2,36 +2,28 @@ import { createContact, deleteContact, getAllContacts, getContactById, updateCon
 import createHttpError from 'http-errors';
 
 export const getContactsController = async (req, res, next) => {
-  try {
-    const contacts = await getAllContacts();
+  const contacts = await getAllContacts();
 
-    res.status(200).json({
-      status: 200,
-      message: "Successfully found contacts!",
-      data: contacts,
-    });
-  } catch (err) {
-    next(err);
-  }
+  res.status(200).json({
+    status: 200,
+    message: "Successfully found contacts!",
+    data: contacts,
+  });
 };
 
 export const getContactsByIdController = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
 
-    if (!contact) {
-      throw createHttpError(404, "Contact not found");
-    }
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
 
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  } catch (err) {
-    next(err);
+  if (!contact) {
+    throw createHttpError(404, "Contact not found");
   }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+  });
 };
 
 export const createContactController = async (req, res) => {
@@ -48,6 +40,8 @@ export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
   const result = await updateContact(contactId, req.body);
 
+  console.log(result);
+  
   if (result === null) {
     next(createHttpError(404, "Contact not found"));
     return;
@@ -56,7 +50,7 @@ export const patchContactController = async (req, res, next) => {
   res.json({
     status: 200,
     message: "Successfully patched a contact!",
-    data: result.contact,
+    data: result,
   });
 };
 
